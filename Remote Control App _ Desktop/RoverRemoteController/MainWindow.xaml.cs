55367e9e9f03cbbd60cab8b68aa8ca9dbd01b259
Oreste Riccardo Natale <o.r.natale@smallrobots.it>
2019-05-19 12:27:32 +0200

    Added initial commit for the Remote Control App (.NET Framework Desktop version)

---
+++ Remote Control App _ Desktop/RoverRemoteController/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿//////////////////////////////////
+// Rover Remote Controller      //
+//                              //
+// Smallrobots.it 2019          //
+//                              //
+// Happily shared under the     //
+// MIT Licence                  //
+//////////////////////////////////
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+using RoverRemoteController.Views;
+
+namespace RoverRemoteController
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Move the window when mouse is down on Canvas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        /// <summary>
+        /// Minimize the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Minimize_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Close the app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Intercept window state changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Maximized:
+                    break;
+                case WindowState.Minimized:
+                    break;
+                case WindowState.Normal:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// It ignores the click, because the control is used just as a readonly indicator
+        /// </summary>
+        private void OnlineIndicator_Click(object sender, RoutedEventArgs e)
+        {
+            ((LedControl)sender).IsChecked = !((LedControl)sender).IsChecked;
+        }
+    }
+
+}
---
+++ Remote Control App _ Desktop/RoverRemoteController/Views/LedControl.cs
@@ -0,0 +1,44 @@
+﻿//////////////////////////////////
+// Rover Remote Controller      //
+//                              //
+// Smallrobots.it 2019          //
+//                              //
+// Happily shared under the     //
+// MIT Licence                  //
+//////////////////////////////////
+///
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RoverRemoteController.Views
+{
+    /// <summary>
+    /// Led control
+    /// </summary>
+    public class LedControl : CheckBox
+    {
+        static LedControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LedControl), new FrameworkPropertyMetadata(typeof(LedControl)));
+        }
+
+        public static readonly DependencyProperty OnColorProperty =
+            DependencyProperty.Register("OnColor", typeof(Brush), typeof(LedControl), new PropertyMetadata(Brushes.Green));
+
+        public Brush OnColor
+        {
+            get { return (Brush)GetValue(OnColorProperty); }
+            set { SetValue(OnColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty OffColorProperty =
+            DependencyProperty.Register("OffColor", typeof(Brush), typeof(LedControl), new PropertyMetadata(Brushes.Red));
+
+        public Brush OffColor
+        {
+            get { return (Brush)GetValue(OffColorProperty); }
+            set { SetValue(OffColorProperty, value); }
+        }
+    }
+}
